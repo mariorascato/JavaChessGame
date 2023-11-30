fc615b9ea09261cfaf8f7af4efbb3c1a59812366
mariolino21714@gmail.com <mariolino21714@gmail.com>
2023-11-30 14:07:39 +0100

    Aggiunte funzionalità di gioco alla classe Pawn e relativi metodi nelle altre classi associate

--- src/main/java/it/unimol/chess/engine/pieces/Pawn.java
+++ src/main/java/it/unimol/chess/engine/pieces/Pawn.java
@@ -11,19 +11,19 @@ import java.util.Collection;
 import java.util.List;
 
 public class Pawn extends Piece{
-    private final static int[] CANDIDATE_MOVE_COORDINATES = {8};
-    Pawn(int piecePosition, Alliance pieceAlliance) {
+    private final static int[] CANDIDATE_MOVE_COORDINATES = {8,16};
+    Pawn(final int piecePosition, final Alliance pieceAlliance) {
         super(piecePosition, pieceAlliance);
     }
 
     @Override
-    public Collection<Move> calculateLegalMoves(Board board) {
+    public Collection<Move> calculateLegalMoves(final Board board) {
 
         final List<Move> legalMoves = new ArrayList<>();
 
         for (final int currentCandidateOffset : CANDIDATE_MOVE_COORDINATES){
 
-            int candidateDestinationCoordinate = this.piecePosition + (this.getPieceAlliance().getDirection() * currentCandidateOffset);
+           final int candidateDestinationCoordinate = this.piecePosition + (this.getPieceAlliance().getDirection() * currentCandidateOffset);
 
             if(!BoardUtils.isValidTileCoordinate(candidateDestinationCoordinate)){
                 continue;
@@ -31,6 +31,15 @@ public class Pawn extends Piece{
             if(currentCandidateOffset == 8 && !board.getTile(candidateDestinationCoordinate).isTileOccupied()){
                 //funzionalità non completa
                 legalMoves.add(new MajorMove(board,this,candidateDestinationCoordinate));
+            }
+            else if(currentCandidateOffset == 16 && this.isFirstMove() &&
+                    (BoardUtils.SECOND_ROW[this.piecePosition] && this.getPieceAlliance().isBlack())
+                    ||(BoardUtils.SEVENTH_ROW[this.piecePosition] && this.getPieceAlliance().isWhite()) ){
+            final int behindCandidateDestinationCoordinate = this.piecePosition + (this.pieceAlliance.getDirection() * 8);
+            if(!board.getTile(behindCandidateDestinationCoordinate).isTileOccupied() &&
+                    !board.getTile(candidateDestinationCoordinate).isTileOccupied()){
+                legalMoves.add(new MajorMove(board,this,candidateDestinationCoordinate));
+            }
             }
 
 
--- src/main/java/it/unimol/chess/engine/pieces/Piece.java
+++ src/main/java/it/unimol/chess/engine/pieces/Piece.java
@@ -9,13 +9,19 @@ import java.util.Collection;
 public abstract class Piece {
     protected final int piecePosition;
     protected final Alliance pieceAlliance;
+    protected final boolean isFirstMove;
     Piece(final int piecePosition,final Alliance pieceAlliance){
         this.piecePosition = piecePosition;
         this.pieceAlliance = pieceAlliance;
+        //TODO altro lavoro da fare
+        this.isFirstMove = false;
     }
     public abstract Collection<Move> calculateLegalMoves(final Board board);
 
     public Alliance getPieceAlliance() {
         return this.pieceAlliance;
+    }
+    public boolean isFirstMove(){
+        return this.isFirstMove;
     }
 }
