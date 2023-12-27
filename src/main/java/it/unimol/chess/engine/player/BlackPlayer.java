c9404eb3758e881f55ed26e4189f60f086b5e275
mariolino21714@gmail.com <mariolino21714@gmail.com>
2023-12-27 11:22:39 +0100

    Continuazione implementazione della logica del castling

--- src/main/java/it/unimol/chess/engine/player/BlackPlayer.java
+++ src/main/java/it/unimol/chess/engine/player/BlackPlayer.java
@@ -1,11 +1,15 @@
 package it.unimol.chess.engine.player;
 
+import com.google.common.collect.ImmutableList;
 import it.unimol.chess.engine.Alliance;
 import it.unimol.chess.engine.board.Board;
 import it.unimol.chess.engine.board.Move;
+import it.unimol.chess.engine.board.Tile;
 import it.unimol.chess.engine.pieces.Piece;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
 public class BlackPlayer extends Player {
     public BlackPlayer(final Board board,
@@ -27,6 +31,40 @@ public class BlackPlayer extends Player {
     @Override
     public Player getOpponent() {
         return this.board.whitePlayer();
+    }
+
+    @Override
+    protected Collection<Move> calculateKingCastles(Collection<Move> playerlegals, Collection<Move> opponentsLegals) {
+        // PARTE NERA DEL KING
+        final List<Move> kingCastels = new ArrayList<>();
+        if(this.playerKing.isFirstMove() && !this.isInCheck()) {
+            if(!this.board.getTile(5).isTileOccupied() && !this.board.getTile(6).isTileOccupied()) {
+                final Tile rookTile = this.board.getTile(7);
+
+                if(rookTile.isTileOccupied() && rookTile.getPiece().isFirstMove()) {
+                    if(Player.calculateAttackOnTile(5,opponentsLegals).isEmpty() &&
+                            Player.calculateAttackOnTile(6, opponentsLegals).isEmpty() &&
+                            rookTile.getPiece().getPieceType().isRook()) {
+                        //TODO AGGIUNGERE A CASTLEMOVE
+                        kingCastels.add(null);
+                    }
+
+                }
+            }
+            if(!this.board.getTile(1).isTileOccupied() &&
+                    !this.board.getTile(2).isTileOccupied() &&
+                    !this.board.getTile(3).isTileOccupied()) {
+
+                final Tile rookTile = this.board.getTile(0);
+                if(rookTile.isTileOccupied() && rookTile.getPiece().isFirstMove()) {
+                    //TODO AGGIUNGERE CASTLEMOVE
+                    kingCastels.add(null);
+                }
+            }
+        }
+
+        return ImmutableList.copyOf(kingCastels);
+    }
     }
 
 }
