759cd74c58db7419d5e199c65ed67ea205491051
mariolino21714@gmail.com <mariolino21714@gmail.com>
2024-01-02 14:51:15 +0100

    Aggiunta highlights delle mosse alla Gui e varie opzioni

--- src/main/java/it/unimol/chess/gui/Table.java
+++ src/main/java/it/unimol/chess/gui/Table.java
@@ -1,5 +1,6 @@
 package it.unimol.chess.gui;
 
+import com.google.common.collect.Lists;
 import it.unimol.chess.engine.board.Board;
 import it.unimol.chess.engine.board.BoardUtils;
 import it.unimol.chess.engine.board.Move;
@@ -10,12 +11,16 @@ import it.unimol.chess.engine.player.MoveTransition;
 import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 
 import static javax.swing.SwingUtilities.isLeftMouseButton;
@@ -28,6 +33,8 @@ public class Table {
     private Tile sourceTile;
     private Tile destinationTile;
     private Piece humanMovedPiece;
+    private BoardDirection boardDirection;
+    private boolean highLightLegalMoves;
     private final static Dimension OUTER_FRAME_DIMENSION = new Dimension(600,600);
     private final static Dimension BOARD_PANEL_DIMENSION = new Dimension(400,350);
     private final static Dimension TILE_PANEL_DIMENSION = new Dimension(10,10);
@@ -43,6 +50,8 @@ public class Table {
         this.gameFrame.setSize(OUTER_FRAME_DIMENSION);
         this.chessBoard = Board.createStandardBoard();
         this.boardPanel = new BoardPanel();
+        this.boardDirection = BoardDirection.NORMAL;
+        this.highLightLegalMoves = false;
         this.gameFrame.add(this.boardPanel, BorderLayout.CENTER);
         this.gameFrame.setVisible(true);
     }
@@ -50,6 +59,7 @@ public class Table {
     private JMenuBar createTableMenuBar() {
         final JMenuBar tableMenuBar = new JMenuBar();
         tableMenuBar.add(createFileMenu());
+        tableMenuBar.add(createPreferencesMenu());
         return tableMenuBar;
     }
     private JMenu createFileMenu() {
@@ -67,6 +77,46 @@ public class Table {
 
         return fileMenu;
     }
+    private JMenu createPreferencesMenu(){
+        final JMenu preferencesMenu = new JMenu("Preferences");
+        final JMenuItem flipBoardMenuItem = new JMenuItem("Flip Board");
+        flipBoardMenuItem.addActionListener(e -> {
+            boardDirection = boardDirection.opposite();
+            boardPanel.drawBoard(chessBoard);
+        });
+        preferencesMenu.add(flipBoardMenuItem);
+
+        preferencesMenu.addSeparator();
+        final JCheckBoxMenuItem legalMoveHighlighterCheckbox = new JCheckBoxMenuItem("Highlight legal Moves",false);
+        legalMoveHighlighterCheckbox.addActionListener(e -> highLightLegalMoves = legalMoveHighlighterCheckbox.isSelected());
+        preferencesMenu.add(legalMoveHighlighterCheckbox);
+        return preferencesMenu;
+    }
+    public enum BoardDirection {
+        NORMAL {
+            @Override
+            List<TilePanel> traverse (final List<TilePanel> boardTiles) {
+                return boardTiles;
+            }
+            @Override
+            BoardDirection opposite() {
+                return FLIPPED;
+            }
+        },
+        FLIPPED {
+            @Override
+            List<TilePanel> traverse(final List<TilePanel> boardTiles) {
+                return Lists.reverse(boardTiles);
+            }
+            @Override
+            BoardDirection opposite() {
+                return NORMAL;
+            }
+        };
+        abstract List<TilePanel> traverse(final List<TilePanel> boardTiles);
+        abstract BoardDirection opposite();
+
+    }
     private class BoardPanel extends JPanel {
         final List<TilePanel> boardTiles;
         BoardPanel() {
@@ -83,7 +133,7 @@ public class Table {
 
         public void drawBoard(final Board board) {
         removeAll();
-        for(final  TilePanel tilePanel : boardTiles) {
+        for(final  TilePanel tilePanel : boardDirection.traverse(boardTiles)) {
             tilePanel.drawTile(board);
             add(tilePanel);
         }
@@ -127,12 +177,7 @@ public class Table {
                     destinationTile = null;
                     humanMovedPiece = null;
                 }
-                SwingUtilities.invokeLater(new Runnable() {
-                    @Override
-                    public void run() {
-                        boardPanel.drawBoard(chessBoard);
-                    }
-                });
+                SwingUtilities.invokeLater(() -> boardPanel.drawBoard(chessBoard));
             }
 
             }
@@ -165,6 +210,7 @@ public class Table {
             assignTilePieceIcon(board);
             validate();
             repaint();
+            highlightLegals(board);
         }
         private void assignTilePieceIcon(final Board board) {
           this.removeAll();
@@ -178,6 +224,25 @@ public class Table {
                   throw new RuntimeException(e);
               }
           }
+        }
+        private void highlightLegals( final Board board)  {
+         if(highLightLegalMoves) {
+             for(final Move move : pieceLegalMoves(board)) {
+                 if(move.getDestinationCoordinate() == this.tileId) {
+                     try {
+                         add(new JLabel(new ImageIcon((ImageIO.read(new File("art/misc/green_dot.png"))))));
+                     } catch (IOException e) {
+                         throw new RuntimeException(e);
+                     }
+                 }
+             }
+         }
+        }
+        private Collection<Move> pieceLegalMoves(final Board board) {
+            if(humanMovedPiece != null && humanMovedPiece.getPieceAlliance() == board.currentPlayer().getAlliance()) {
+                return  humanMovedPiece.calculateLegalMoves(board);
+            }
+            return Collections.emptyList();
         }
 
         private void assignTileColor() {
