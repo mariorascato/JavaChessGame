b4d439f3dcb5ba36d1b14f5615c102822ffa86e5
mariolino21714@gmail.com <mariolino21714@gmail.com>
2024-01-05 13:00:41 +0100

    Migliorata la funzione di MinMax

--- src/main/java/it/unimol/chess/engine/player/ai/StandardBoardEvaluator.java
+++ src/main/java/it/unimol/chess/engine/player/ai/StandardBoardEvaluator.java
@@ -5,6 +5,11 @@ import it.unimol.chess.engine.pieces.Piece;
 import it.unimol.chess.engine.player.Player;
 
 public final class StandardBoardEvaluator implements BoardEvaluator {
+    private static final int CHECK_BONUS = 50;
+    private static final int CHECK_MATE_BONUS = 10000;
+    private static final int DEPTH_BONUS = 100;
+    private static final int CASTLE_BONUS = 60;
+
     @Override
     public int evaluate(final Board board,
                         final int depth) {
@@ -15,8 +20,32 @@ public final class StandardBoardEvaluator implements BoardEvaluator {
     private int scorePlayer(final Board board,
                             final Player player,
                             final int depth) {
-        return pieceValue(player);
+        return pieceValue(player) + mobility(player) + check(player)
+                + checkMate(player,depth) + castled(player);
     }
+
+    private static int castled(Player player) {
+        return player.isCastled() ? CASTLE_BONUS : 0;
+    }
+
+    private static int checkMate(Player player, int depth) {
+        return player.getOpponent().isInCheckMate() ? CHECK_MATE_BONUS * depthBonus(depth) : 0;
+    }
+
+    private static int depthBonus(int depth) {
+        return depth == 0 ? 1 : DEPTH_BONUS * depth;
+    }
+
+
+
+    private static int check(Player player) {
+        return player.getOpponent().isInCheck() ? CHECK_BONUS : 0;
+    }
+
+    private static int mobility(Player player) {
+        return player.getLegalMoves().size();
+    }
+
     private static int pieceValue(final Player player) {
         int pieceValueScore = 0;
         for(final Piece piece : player.getActivePieces()) {
