6ac36814da0ecfab008d6540622eb9b5bc377ec9
mariolino21714@gmail.com <mariolino21714@gmail.com>
2024-01-05 14:28:31 +0100

    Completamento MiniMax ed aggiunta di un caso di test

--- src/main/java/it/unimol/chess/engine/player/ai/MiniMax.java
+++ src/main/java/it/unimol/chess/engine/player/ai/MiniMax.java
@@ -6,8 +6,10 @@ import it.unimol.chess.engine.player.MoveTransition;
 
 public class MiniMax implements MoveStrategy{
     private final BoardEvaluator boardEvaluator;
-    public MiniMax() {
+    private final int searchDepth;
+    public MiniMax(final int searchDepth) {
         this.boardEvaluator = new StandardBoardEvaluator();
+        this.searchDepth = searchDepth;
     }
 
     @Override
@@ -15,7 +17,7 @@ public class MiniMax implements MoveStrategy{
         return "MiniMax";
     }
     @Override
-    public Move execute(Board board, int depth) {
+    public Move execute(Board board) {
 
         final long  startTime = System.currentTimeMillis();
         Move bestMove = null;
@@ -24,7 +26,7 @@ public class MiniMax implements MoveStrategy{
         int lowestSeenValue = Integer.MAX_VALUE;
         int currentValue;
 
-        System.out.println(board.currentPlayer() + "THINKING with depth = " + depth);
+        System.out.println(board.currentPlayer() + "THINKING with depth = " + this.searchDepth);
 
         int numMoves = board.currentPlayer().getLegalMoves().size();
 
@@ -32,8 +34,8 @@ public class MiniMax implements MoveStrategy{
             final MoveTransition moveTransition = board.currentPlayer().makeMove(move);
             if(moveTransition.getMoveStatus().isDone()) {
                 currentValue = board.currentPlayer().getAlliance().isWhite() ?
-                        min(moveTransition.getTransitionBoard(), depth - 1 ) :
-                        max(moveTransition.getTransitionBoard(), depth - 1);
+                        min(moveTransition.getTransitionBoard(), this.searchDepth - 1 ) :
+                        max(moveTransition.getTransitionBoard(), this.searchDepth - 1);
 
                 if(board.currentPlayer().getAlliance().isWhite() && currentValue >= highestSeenValue) {
                     highestSeenValue = currentValue;
@@ -64,9 +66,13 @@ public class MiniMax implements MoveStrategy{
     }
     return lowestSeenValue;
     }
+    private static boolean isEndGameScenario(final Board board){
+        return board.currentPlayer().isInCheckMate() ||
+               board.currentPlayer().isInStaleMate();
+    }
     public int max(final Board board,
                    final int depth) {
-        if(depth == 0)
+        if(depth == 0 || isEndGameScenario(board))
             return this.boardEvaluator.evaluate(board,depth);
         int highestSeenValue = Integer.MIN_VALUE;
         for(final  Move move : board.currentPlayer().getLegalMoves()) {
--- src/test/java/it/unimol/chess/tests/TestBoard.java
+++ src/test/java/it/unimol/chess/tests/TestBoard.java
@@ -1,10 +1,52 @@
 package it.unimol.chess.tests;
 
+import it.unimol.chess.engine.board.Board;
+import it.unimol.chess.engine.board.BoardUtils;
+import it.unimol.chess.engine.board.Move;
+import it.unimol.chess.engine.player.MoveTransition;
+import it.unimol.chess.engine.player.ai.MiniMax;
+import it.unimol.chess.engine.player.ai.MoveStrategy;
 import org.junit.Test;
+
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class TestBoard {
     @Test
-    public void initalBoard(){
+    public void testFoolsMate(){
+        final Board board = Board.createStandardBoard();
+        final MoveTransition t1 = board.currentPlayer()
+                .makeMove(Move.MoveFactory.createMove(board, BoardUtils.getCoordinateAtPosition("f2"),
+                        BoardUtils.getCoordinateAtPosition("f3")));
+        assertTrue(t1.getMoveStatus().isDone());
+
+        final MoveTransition t2 = t1.getTransitionBoard()
+                .currentPlayer()
+                .makeMove(Move.MoveFactory.createMove(t1.getTransitionBoard(),BoardUtils.getCoordinateAtPosition("e7"),
+                        BoardUtils.getCoordinateAtPosition("e5")));
+        assertTrue(t2.getMoveStatus().isDone());
+
+        final MoveTransition t3 = t2.getTransitionBoard().
+                currentPlayer().
+                makeMove(Move.MoveFactory.createMove(t2.getTransitionBoard(),BoardUtils.getCoordinateAtPosition("g2"),
+                        BoardUtils.getCoordinateAtPosition("g4")));
+
+        assertTrue(t3.getMoveStatus().isDone());
+
+        final MoveStrategy strategy = new MiniMax(4);
+        final Move aiMove = strategy.execute(t3.getTransitionBoard());
+
+        final Move bestMove = Move.MoveFactory.createMove(t3.getTransitionBoard(),BoardUtils.getCoordinateAtPosition("d8"),
+                BoardUtils.getCoordinateAtPosition("h4"));
+
+        assertEquals(aiMove, bestMove);
+
+
+
+
+
+
 
     }
 
